5212df7194b92270103acc0ef214e2e8aaa74eb5
TELUS\x194594 <dong.yao@telus.com>
2017-08-30 11:48:27 -0700

    remove unused code in dataHealth.py

--- quote/dataHealth.py
+++ quote/dataHealth.py
@@ -22,7 +22,10 @@ def checkAndUpdate(symbol, date=dt.datetime.now()):
         if "nextClose" not in quotes[1] or "nextClosePercentage" not in quotes[1] or quotes[1]["nextClose"] is None or quotes[1]["nextClosePercentage"] is None :
             quotes[1]['nextClose'] = quotes[0]['Close']
             quotes[1]['nextClosePercentage'] = (quotes[0]['Close'] - quotes[1]['Close']) / quotes[1]['Close']
-            stockMongo.updateQuoteNextClose(quotes[1]["_id"], quotes[1]['nextClose'], quotes[1]['nextClosePercentage'])
+            print('the nextClose/nextClosePercentage is Null... ')
+            print(quotes)
+            print('...')
+#             stockMongo.updateQuoteNextClose(quotes[1]["_id"], quotes[1]['nextClose'], quotes[1]['nextClosePercentage'])
 
 def isPreviousNextCloseUpdated(date):
     symbols = stockMongo.findAllActiveSymbols(); 
@@ -59,54 +62,6 @@ def dailyCheck(date):
     isPreviousPredictionResultUpdated(date)
     
     print('data load is good...')
-
-# this method is to eliminate bad data
-def removeDuplicate():
-    duplicates = stockMongo.findDuplicatedQuotes()
-    df = pd.DataFrame(list(duplicates))
-    for item in df['_id'].values:
-        symbol = item['Symbol']
-        date = item['Date']
-        quotes = stockMongo.findQuotesBySymbolDate(symbol, date)
-        quotes_df = pd.DataFrame(list(quotes))
-        
-        # if nextClose or nextClosePercentage is null, put in the values from previous
-        # nextCloseNotNull_df = quotes_df.loc[lambda df: pd.notnull(df.nextClose),:]
-        # nextCloseNull_df = quotes_df.loc[lambda df: pd.isnull(df.nextClose)]
-        for i in range(1, len(quotes_df)):
-            if pd.isnull(quotes_df.loc[i, 'nextClose']) : 
-                quotes_df.loc[i, 'nextClose'] = quotes_df.loc[i - 1, 'nextClose']
-                quotes_df.loc[i, 'nextClosePercentage'] = quotes_df.loc[i - 1, 'nextClosePercentage']
-                stockMongo.updateQuoteNextClose(quotes_df.loc[i, '_id'], quotes_df.loc[i - 1, 'nextClose'], quotes_df.loc[i - 1, 'nextClosePercentage'])
-        
-        quotes = stockMongo.findQuotesBySymbolDate(symbol, date)
-        quotes_df = pd.DataFrame(list(quotes))
-        null_df = quotes_df.loc[lambda df: pd.isnull(df.Open) | pd.isnull(df.Close) | pd.isnull(df.Volume) | df.Open == 0, :]
-                
-        for id in null_df['_id'].values:
-            stockMongo.deleteById(id)
-            
-            
-    duplicates = stockMongo.findDuplicatedQuotes()
-    df = pd.DataFrame(list(duplicates))
-    for item in df['_id'].values:
-        symbol = item['Symbol']
-        date = item['Date']
-        quotes = stockMongo.findQuotesBySymbolDate(symbol, date)
-        quotes_df = pd.DataFrame(list(quotes))
-        for i in range(0, len(quotes_df) - 1) :
-            stockMongo.deleteById(quotes_df.loc[i, '_id'])
-    
-    # both have full data, delete the first one, keep the last one
-    duplicates = stockMongo.findDuplicatedQuotes()
-    df = pd.DataFrame(list(duplicates))
-    print(df)
-    
-    
-def updateSymbolStatusToInactive(symbol, df):
-    if len(df) == 0:
-        stockMongo.addSymbolStatus(symbol, stockMongo.SYMBOL_INACTIVE);
-        return;
 
 
 def deleteDuplicatedPrediction(mode):
